18f6687f42baae60a472ce7d59f5a9172683c5e1
sumiransingh-droid <65755901+sumiransingh-droid@users.noreply.github.com>
2024-04-15 09:42:49 +0530

    Delete OTC-dummytask/otc-task directory

--- OTC-dummytask/otc-task/src/app/constant/api-constant.ts
+++
@@ -1,8 +0,0 @@
-export enum ApiMethod {  GET = "GET",  POST = "POST",}
-export const AuthEndPoints: any = {  
-    REGISTER_USER: "/usercategories",
-    SubUserCategories : "/usersubcategories" ,
-    signup : "/signup",
-    otpVerify:"/signup/otpverify"
-
-}
--- OTC-dummytask/otc-task/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  recaptcha: {
-    siteKey: "6LceT1klAAAAADS3KOYVpKJ-mPYFCzISO8Iw6j8a",
-  },
-    production: false,
-    BaseUrl: "https://dev-api.newage.market/platform/api",
-  };
